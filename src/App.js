f3ada9f8ae328f9dba3f4ead334763730af6a497
JoseVitorNobre <josevitortinf@gmail.com>
2022-03-23 09:41:35 -0300

    Atividade 01

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Questao1 from './components/atividade/Questao1';
+import Questao2 from './components/atividade/Questao2';
+import Questao4 from './components/atividade/Questao4';
+function App() {
+  return (
+    <div className="App">
+      <Questao4/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/atividade/Questao4.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Questao2 from "./Questao2";
+class Questao4 extends Component{
+    render(){
+        return (
+            <Questao2
+                nome="José Vitor de Oliveira Nobre"
+                curso="Engenharia de Software"
+                cidOri="Fortaleza" />
+        )
+    }
+}
+export default Questao4
