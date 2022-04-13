29315d931af1398adf23f7f0513f2e00ae083039
JoseVitorNobre <josevitortinf@gmail.com>
2022-04-13 09:57:36 -0300

    CRUD com navegacao

--- Atividade06/src/App.js
+++ Atividade06/src/App.js
@@ -5,8 +5,13 @@ import { Routes, Route, Link } from "react-router-dom";
 
 import Home from './components/Home';
 import About from './components/About';
-import Page1 from './components/Page1';
-import Page2 from './components/Page2';
+// Criamos 2 pastas + 3 arquivos
+import CreateStudent from './components/crud/students/CreateStudent';
+import ListStudent from './components/crud/students/ListStudent';
+import EditStudent from './components/crud/students/EditStudent';
+// Removemos o Page1 e o Page2 e no lugar colocamos os CreateStudent, ListStudent etc.
+// import Page1 from './components/Page1';
+// import Page2 from './components/Page2';
 const App = () =>{
     return(
         <div className='container'>
@@ -16,16 +21,18 @@ const App = () =>{
                     <ul className="navbar-nav mr-auto">
                         <li className="navitem"><Link to="/" className="nav-link">Home</Link></li>
                         <li className="navitem"><Link to="about" className="nav-link">Sobre</Link></li>
-                        <li className="navitem"><Link to="page1" className="nav-link">Pagina 1</Link></li>
-                        <li className="navitem"><Link to="page2/Vitor" className="nav-link">Pagina 2</Link></li>
+                        <li className="navitem"><Link to="createStudent" className="nav-link">Criar Estudante</Link></li>
+                        <li className="navitem"><Link to="listStudent" className="nav-link">Listar Estudante</Link></li>
+                        {/* Não criamos o li do editStudent pq ele vai ser chamado no listStudent */}
                     </ul>
                 </div>
             </nav>
             <Routes>
                 <Route path="/" element={<Home/>} />
                 <Route path="about" element={<About/>} />
-                <Route path="page1" element={<Page1/>} />
-                <Route path="page2/:nome" element={<Page2/>} />
+                <Route path="createStudent" element={<CreateStudent/>} />
+                <Route path="listStudent" element={<ListStudent/>} />
+                <Route path="editStudent/:id" element={<EditStudent/>} />
             </Routes>
       </div>
     )
---
+++ Atividade06/src/components/crud/students/data.js
@@ -0,0 +1,9 @@
+//Aqui é uma simulação de uma tabela de dados
+const students = [
+    {id:0, name: "Jose Vitor", course:"Engenharia de Software",ira:9.2},
+    {id:1, name: "Higor Santiago", course:"Engenharia de Software",ira:9.2},
+    {id:2, name: "Daniel Almeida", course:"Engenharia de Software",ira:9.4},
+    {id:3, name: "Alexson Almeida", course:"Engenharia de Software",ira:9.5}
+]
+
+export {students};
